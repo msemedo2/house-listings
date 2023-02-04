b45e419711f2c9bc25ededdae7a389838ad48be3
unknown <miguel.semedo.2@outlook.com>
2023-02-04 22:08:04 +0000

    create search field component design

---
+++ src/components/SearchField.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="input-container">
+		<img
+			class="magnify-img"
+			src="../../assets/ic_search@3x.png"
+			alt="magnifying glass"
+		/>
+		<input type="text" placeholder="Search for a house" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SearchField',
+};
+</script>
+
+<style scoped>
+.input-container {
+	background-color: var(--tertiary-color-light);
+	padding: 5px 5px 5px 5px;
+	border-radius: 5px;
+}
+.magnify-img {
+	width: 15px;
+	margin-left: 10px;
+	position: relative;
+	top: 3px;
+}
+
+input {
+	width: 330px;
+	margin-left: 10px;
+}
+</style>
