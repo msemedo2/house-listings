3ee968b4135cb1238d2a10b70de5e0bcde84f2b2
unknown <miguel.semedo.2@outlook.com>
2023-02-05 10:16:09 +0000

    fix header position

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,8 @@
 		<nav class="navbar">
 			<img src="../../assets/img_logo_dtt@3x.png" alt="logo" sizes />
 			<ul>
-				<router-link class="nav-link" :to="{ name: 'home' }"
-					>Houses</router-link
-				>
-				<router-link class="nav-link" :to="{ name: 'about' }"
-					>About</router-link
-				>
+				<router-link class="nav-link" :to="{ name: 'home' }">Houses</router-link>
+				<router-link class="nav-link" :to="{ name: 'about' }">About</router-link>
 			</ul>
 		</nav>
 	</header>
@@ -25,7 +21,9 @@ header {
 	width: 100%;
 	height: 55px;
 	background-color: white;
+	position: fixed;
 }
+
 .navbar {
 	margin: 0 auto;
 	height: 100%;
@@ -35,6 +33,7 @@ header {
 	flex-direction: row;
 	align-items: center;
 }
+
 .nav-link {
 	display: inline-block;
 	cursor: pointer;
