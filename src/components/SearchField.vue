335f6c20ebbf2ab8dc608f03eec7771487186670
unknown <miguel.semedo.2@outlook.com>
2023-02-05 15:28:41 +0000

    input field set to store the search value and dispatch to store

--- src/components/SearchField.vue
+++ src/components/SearchField.vue
@@ -1,17 +1,25 @@
 <template>
 	<div class="input-container">
-		<img
-			class="magnify-img"
-			src="../../assets/ic_search@3x.png"
-			alt="magnifying glass"
-		/>
-		<input type="text" placeholder="Search for a house" />
+		<img class="magnify-img" src="../../assets/ic_search@3x.png" alt="magnifying glass" />
+		<input type="text" placeholder="Search for a house" v-model="searchValue" @input="updateSearchValue" />
 	</div>
 </template>
 
 <script>
+
 export default {
 	name: 'SearchField',
+	data() {
+		return {
+			searchValue: '',
+		}
+	},
+	//update search value in store
+	methods: {
+		updateSearchValue() {
+			this.$store.dispatch('updateSearchValue', this.searchValue)
+		}
+	}
 };
 </script>
 
@@ -21,6 +29,7 @@ export default {
 	padding: 5px 5px 5px 5px;
 	border-radius: 5px;
 }
+
 .magnify-img {
 	width: 15px;
 	margin-left: 10px;
