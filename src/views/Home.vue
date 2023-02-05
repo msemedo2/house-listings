b7a1ad59c6647099025ef05fed2d75f6197d28bf
unknown <miguel.semedo.2@outlook.com>
2023-02-05 15:30:01 +0000

    update button change color to color variable

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,46 +10,48 @@
 		<div class="options-container">
 			<search-field />
 			<div class="filter-container">
-				<button
-					class="price-button"
-					:style="{ backgroundColor: buttonColors[0] }"
-					@click="sortSelection(0)"
-				>
+				<button class="price-button" :style="{ backgroundColor: buttonColors[0] }" @click="sortSelection(0)">
 					Price
 				</button>
-				<button
-					class="size-button"
-					:style="{ backgroundColor: buttonColors[1] }"
-					@click="sortSelection(1)"
-				>
+				<button class="size-button" :style="{ backgroundColor: buttonColors[1] }" @click="sortSelection(1)">
 					Size
 				</button>
+				<p>{{ searchValue }}</p>
 			</div>
+
 		</div>
+
+		<!-- House Listings -->
+		<house-list />
 	</div>
 </template>
 
 <script>
 import AppButton from '@/components/AppButton.vue';
 import SearchField from '@/components/SearchField.vue';
+import HouseList from '../components/HouseList.vue';
 
 export default {
 	name: 'Home',
 	components: {
 		AppButton,
 		SearchField,
+		HouseList,
 	},
 	data() {
 		return {
-			buttonColors: ['#eb5440', '#c3c3c3'],
+			buttonColors: ['var(--primary-color)', 'var(--tertiary-color-dark)'],
+			searchValue: '',
 		};
 	},
 	methods: {
+		// revert button color when opposite button is selected
 		sortSelection(index) {
-			const newButtonColors = ['#c3c3c3', '#c3c3c3'];
-			newButtonColors[index] = '#eb5440';
+			const newButtonColors = ['var(--tertiary-color-dark)', 'var(--tertiary-color-dark)'];
+			newButtonColors[index] = 'var(--primary-color)';
 			this.buttonColors = newButtonColors;
 		},
+
 	},
 };
 </script>
@@ -67,6 +69,7 @@ export default {
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
+	margin-top: 80px;
 }
 
 /* --- */
@@ -82,9 +85,11 @@ export default {
 	height: 100%;
 	background-color: var(--tertiary-color-dark);
 }
+
 .price-button {
 	border-radius: 8px 0 0 8px;
 }
+
 .size-button {
 	border-radius: 0 10px 10px 0;
 }
