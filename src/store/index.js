33aec96c13ff9147b9531cd531e77a9bba82dca5
unknown <miguel.semedo.2@outlook.com>
2023-02-05 15:31:16 +0000

    store search value in store

--- src/store/index.js
+++ src/store/index.js
@@ -4,13 +4,18 @@ import axios from 'axios';
 export default createStore({
 	state: {
 		houses: [],
+		searchValue: '',
 	},
 	mutations: {
 		SET_HOUSES(state, houses) {
 			state.houses = houses;
 		},
+		UPDATE_SEARCH_VALUE(state, searchValue) {
+			state.searchValue = searchValue;
+		},
 	},
 	actions: {
+		// Fetch house Listings from the Api
 		fetchHouses({ commit }) {
 			const url = 'https://api.intern.d-tt.nl/api/houses';
 			const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
@@ -24,6 +29,9 @@ export default createStore({
 				.then((res) => {
 					commit('SET_HOUSES', res.data);
 				});
+		},
+		updateSearchValue({ commit }, searchValue) {
+			commit('UPDATE_SEARCH_VALUE', searchValue);
 		},
 	},
 });
