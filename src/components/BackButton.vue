6011bb5e4b37a97ed4edee805348ef7bda63f933
unknown <miguel.semedo.2@outlook.com>
2023-02-07 05:49:39 +0000

    create back button component

---
+++ src/components/BackButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <router-link class="go-back-container" :to="{ name: 'home' }"><img class="arrow-image"
+      src="../../assets/ic_back_grey@3x.png" alt="back arrow">Back to overview</router-link>
+</template>
+
+<script>
+export default {
+  name: 'BackButton',
+}
+</script>
+
+<style scoped>
+.arrow-image {
+  width: 20px;
+  margin-right: 20px;
+  position: relative;
+  top: 3px;
+}
+</style>
