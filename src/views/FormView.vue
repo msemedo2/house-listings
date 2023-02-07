cd1a3614996660552d972eeb02a6fd3060ed6028
unknown <miguel.semedo.2@outlook.com>
2023-02-07 21:25:45 +0000

    add form validation. error border and message. create functionality for posting a new house

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -7,13 +7,17 @@
 
         <!-- Street Name -->
         <label for="street-name" class="input-title">Street name*</label>
-        <input type="text" name="streetName" placeholder="Enter the street name" v-model="listing.streetName">
+        <input type="text" name="streetName" placeholder="Enter the street name" v-model="listing.streetName"
+          :class="{ 'error': isFieldRequired }">
+        <span v-if="isFieldRequired" class="error-message">Required field is missing.</span>
         <div class="house-info-container">
 
           <!-- House Number -->
           <div class="house-info">
             <label for="house-number" class="input-title">House number*</label>
-            <input type="text" name="houseNumber" placeholder="Enter house number" v-model="listing.houseNumber">
+            <input type="text" name="houseNumber" placeholder="Enter house number" v-model="listing.houseNumber"
+              :class="{ 'error': isFieldRequired }">
+            <span v-if="isFieldRequired" class="error-message">Required field is missing.</span>
           </div>
 
           <!-- Additional -->
@@ -25,37 +29,48 @@
 
         <!-- Zip -->
         <label for="postal-code" class="input-title">Postal code*</label>
-        <input type="text" name="zip" placeholder="e.g. 1000 AA" v-model="listing.zip">
+        <input type="text" name="zip" placeholder="e.g. 1000 AA" v-model="listing.zip"
+          :class="{ 'error': isFieldRequired }">
+        <span v-if="isFieldRequired" class="error-message">Required field is missing.</span>
 
         <!-- City -->
         <label for="city" class="input-title">City*</label>
-        <input type="text" name="city" placeholder="e.g. Utrecht" v-model="listing.city">
+        <input type="text" name="city" placeholder="e.g. Utrecht" v-model="listing.city"
+          :class="{ 'error': isFieldRequired }">
+        <span v-if="isFieldRequired" class="error-message">Required field is missing.</span>
 
         <!-- Upload Picture -->
         <label for="upload-picture" class="input-title">Upload picture(PNG or JPG)*</label>
-        <div class="upload-picture-container" :style="{ border: uploadedImage !== '' ? 'none' : null }">
-          <input type="file" class="upload-input" accept="image/jpeg, image/png" @change="uploadImage"
-            ref="uploadInput">
+        <span v-if="isFieldRequired" class="error-message">Required field is missing.</span>
+        <div class="upload-picture-container" :style="{ border: displayImage !== '' ? 'none' : null }">
+          <input type="file" class="upload-input" accept="image/jpeg, image/png" @change="uploadImage" ref="uploadInput"
+            :class="{ 'error': isFieldRequired }">
           <img v-show="uploadedImage === ''" :src="plusImage" alt="plus" class="plus" @click="triggerUploadInput">
-          <img v-show="uploadedImage !== ''" :src="uploadedImage" alt="uploaded image" class="upload-image"
+          <img v-show="displayImage !== ''" :src="displayImage" alt="uploaded image" class="display-image"
             @click="triggerUploadInput">
         </div>
 
         <!-- Price -->
         <label for="price" class="input-title">Price*</label>
-        <input type="text" name="price" placeholder="e.g. €150.000" v-model="listing.price">
+        <input type="text" name="price" placeholder="e.g. €150.000" v-model="listing.price"
+          :class="{ 'error': isFieldRequired }">
+        <span v-if="isFieldRequired" class="error-message">Required field is missing.</span>
         <div class="house-info-container">
           <div class="house-input">
 
             <!-- Size -->
             <label for="size" class="input-title">Size*</label>
-            <input type="text" name="size" placeholder="e.g. 60m2" v-model="listing.size">
+            <input type="text" name="size" placeholder="e.g. 60m2" v-model="listing.size"
+              :class="{ 'error': isFieldRequired }">
+            <span v-if="isFieldRequired" class="error-message">Required field is missing.</span>
           </div>
 
           <!-- Garage -->
           <div class="house-input">
             <label for="garage" class="input-title">Garage*</label>
-            <input type="number" name="hasGarage" placeholder="Select" min="0" max="1" v-model="listing.hasGarage">
+            <input type="number" name="hasGarage" placeholder="Select" min="0" max="1" v-model="listing.hasGarage"
+              :class="{ 'error': isFieldRequired }">
+            <span v-if="isFieldRequired" class="error-message">Required field is missing.</span>
           </div>
         </div>
         <div class="house-info-container">
@@ -63,28 +78,35 @@
 
             <!-- Bedrooms -->
             <label for="bedrooms" class="input-title">Bedrooms*</label>
-            <input type="text" name="bedrooms" placeholder="Enter amount" v-model="listing.bedrooms">
+            <input type="text" name="bedrooms" placeholder="Enter amount" v-model="listing.bedrooms"
+              :class="{ 'error': isFieldRequired }">
+            <span v-if="isFieldRequired" class="error-message">Required field is missing.</span>
           </div>
           <div class="house-input">
 
             <!-- Bathrooms -->
             <label for="bathrooms" class="input-title">Bathrooms*</label>
-            <input type="text" name="bathrooms" placeholder="Enter amount" v-model="listing.bathrooms">
+            <input type="text" name="bathrooms" placeholder="Enter amount" v-model="listing.bathrooms"
+              :class="{ 'error': isFieldRequired }">
+            <span v-if="isFieldRequired" class="error-message">Required field is missing.</span>
           </div>
         </div>
 
         <!-- Construction Date -->
         <label for="construction-date" class="input-title">Construction date*</label>
-        <input type="text" name="constructionYear" placeholder="e.g. 1990" v-model="listing.constructionYear">
+        <input type="text" name="constructionYear" placeholder="e.g. 1990" v-model="listing.constructionYear"
+          :class="{ 'error': isFieldRequired }">
+        <span v-if="isFieldRequired" class="error-message">Required field is missing.</span>
 
         <!-- Description -->
         <label for="description" class="input-title">Description*</label>
-        <textarea name="description" cols="30" rows="7" placeholder="Enter description"
-          v-model="listing.description"></textarea>
+        <textarea name="description" cols="30" rows="7" placeholder="Enter description" v-model="listing.description"
+          :class="{ 'error': isFieldRequired }"></textarea>
+        <span v-if="isFieldRequired" class="error-message">Required field is missing.</span>
 
         <!-- Submit Button -->
         <div class="submit-button-container">
-          <button class="submit-button" type="submit" @click="postHouse">Post</button>
+          <button class="submit-button" type="submit" @click="checkRequiredFields">Post</button>
         </div>
       </div>
     </div>
@@ -102,6 +124,7 @@ export default {
   data() {
     return {
       plusImage: '../../assets/ic_upload@3x.png',
+      displayImage: '',
       uploadedImage: '',
       listing: {
         price: '',
@@ -116,7 +139,8 @@ export default {
         constructionYear: '',
         hasGarage: '',
         description: '',
-      }
+      },
+      isFieldRequired: false,
     }
   },
   components: {
@@ -129,21 +153,68 @@ export default {
     triggerUploadInput() {
       this.$refs.uploadInput.click();
     },
+
+    //Convert image file into file that can be read and assign it to uploaded image
     uploadImage(e) {
-      // it will only upload image if file chosen
-      if (e.target.files.length !== 0) {
-        this.uploadedImage = URL.createObjectURL(e.target.files[0]);
-      }
+      const files = e.target.files
+      const file = files[0];
+      this.uploadedImage = file;
+      console.log(this.uploadedImage)
+
+      // const files = e.target.files
+      const fileReader = new FileReader()
+      fileReader.addEventListener('load', () => {
+        this.displayImage = fileReader.result
+      })
+      fileReader.readAsDataURL(files[0])
     },
-    setListingInfo() {
-      this.$store.dispatch('setListingInfo', this.listing)
-    },
-    postHouse(e) {
+
+    checkRequiredFields(e) {
       e.preventDefault();
+      const requiredFields = Object.entries(this.listing);
+      for (const [field, value] of requiredFields) {
+        if (field === 'numberAddition') continue;
+        if (!value && field !== 'hasGarage') {
+          this.isFieldRequired = true;
+          return;
+        }
+        if (field === 'hasGarage' && (value !== 0 && value !== 1)) {
+          console.error(`The ${field} field must be 0 or 1`);
+          return;
+        }
+        if (!this.displayImage) {
+          console.error(`The displayImage field is required`);
+          return;
+        }
+      }
+      this.postHouse()
+    },
+
+    postImage(id) {
+      const imgUrl = `https://api.intern.d-tt.nl/api/houses/${id}/upload`;
+      const API_KEY = 'QftPEp38KycCIOjqmsBra-XeVk7_hlAN';
+
+      const formData = new FormData();
+      formData.append("image", this.uploadedImage);
+
+      axios
+        .post(imgUrl, formData, {
+          headers: {
+            'X-Api-Key': API_KEY,
+            'Content-Type': 'multipart/form-data',
+          },
+        }).then((res) => {
+          console.log(this.houses)
+          console.log(res.data)
+          this.$store.dispatch('fetchHouses')
+          this.$router.push({ path: `/house/${id}` })
+        })
+    },
+    postHouse() {
       const url = 'https://api.intern.d-tt.nl/api/houses';
       const API_KEY = 'QftPEp38KycCIOjqmsBra-XeVk7_hlAN';
       const data = this.listing;
-      console.log(data);
+
       axios
         .post(url, data, {
           headers: {
@@ -151,11 +222,16 @@ export default {
           },
         })
         .then((res) => {
-          this.houses = [...this.houses, res.data]
+          const id = res.data.id;
+          this.houses = [...this.houses]
+          this.postImage(id)
+
+
         })
         .catch((err) => {
           console.log(err);
         });
+
     }
   }
 };
@@ -249,7 +325,7 @@ input[type=number]::-webkit-inner-spin-button {
 }
 
 .plus,
-.upload-image {
+.display-image {
   position: absolute;
   top: 50%;
   left: 50%;
@@ -295,4 +371,17 @@ textarea {
 .error {
   border: 1px solid var(--primary-color);
 }
+
+.error::placeholder {
+  color: var(--primary-color);
+}
+
+.error-message {
+  font-style: italic;
+  font-size: var(--fs-input);
+  font-weight: var(--fw-medium);
+  color: var(--primary-color);
+}
 </style>
+
+
