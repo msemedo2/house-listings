63a5d8d5c39caaf8a7cd0ddc93b4a0a11a109738
unknown <miguel.semedo.2@outlook.com>
2023-02-07 10:51:10 +0000

    save selected house id on vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -6,7 +6,9 @@ export default createStore({
 		houses: [],
 		searchValue: '',
 		activeSortButton: '',
+		selectedHouseId: '',
 	},
+
 	mutations: {
 		SET_HOUSES(state, houses) {
 			state.houses = houses;
@@ -17,12 +19,17 @@ export default createStore({
 		SET_ACTIVE_SORT_BUTTON(state, sortButton) {
 			state.activeSortButton = sortButton;
 		},
+		SET_SELECTED_HOUSE_ID(state, selectedHouseId) {
+			state.selectedHouseId = selectedHouseId;
+		},
 	},
+
 	actions: {
 		// Fetch house Listings from the Api
 		fetchHouses({ commit }) {
 			const url = 'https://api.intern.d-tt.nl/api/houses';
 			const API_KEY = 'QftPEp38KycCIOjqmsBra-XeVk7_hlAN';
+
 			axios
 				.get(url, {
 					headers: {
@@ -33,11 +40,17 @@ export default createStore({
 					commit('SET_HOUSES', res.data);
 				});
 		},
+
 		updateSearchValue({ commit }, searchValue) {
 			commit('UPDATE_SEARCH_VALUE', searchValue);
 		},
+
 		setActivateSortButton({ commit }, sortButton) {
 			commit('SET_ACTIVE_SORT_BUTTON', sortButton);
+		},
+
+		setSelectedHouseId({ commit }, selectedHouseId) {
+			commit('SET_SELECTED_HOUSE_ID', selectedHouseId);
 		},
 	},
 });
