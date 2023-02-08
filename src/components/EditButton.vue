ba407dc8c01119b28e819fd058394ef015559953
unknown <miguel.semedo.2@outlook.com>
2023-02-08 11:20:20 +0000

    update eddit button to trigger the delete modal when selected delete button

---
+++ src/components/EditButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <img src="../../assets/ic_edit@3x.png" alt="location icon" class="edit-icon">
+  <img src="../../assets/ic_delete@3x.png" alt="location icon" class="delete-icon" @click.prevent="setActiveModal">
+</template>
+
+<script>
+
+export default {
+  name: 'EditButton',
+
+  methods: {
+    setActiveModal() {
+      this.$store.dispatch('setActiveModal', true)
+    },
+
+  },
+}
+</script>
+
+<style scoped>
+.delete-icon,
+.edit-icon {
+  width: 20px;
+  margin-left: 25px;
+  cursor: pointer;
+}
+</style>
