1ebd528a25a8c0615d62b58154516547c190b3eb
unknown <miguel.semedo.2@outlook.com>
2023-02-09 17:56:33 +0000

    create edit button component for house details page

--- src/components/EditButton.vue
+++ src/components/EditButton.vue
@@ -1,18 +1,27 @@
 <template>
-  <img src="../../assets/ic_edit@3x.png" alt="location icon" class="edit-icon">
-  <img src="../../assets/ic_delete@3x.png" alt="location icon" class="delete-icon" @click.prevent="setActiveModal">
+  <div v-show="!listing.madeByMe === false" class="buttons-container">
+    <router-link :to="{ path: `/edit-listing/${selectedHouseId}` }">
+      <img src="../../assets/ic_edit@3x.png" alt="location icon" class="edit-icon" @click="setListing">
+    </router-link>
+    <img src="../../assets/ic_delete@3x.png" alt="location icon" class="delete-icon" @click.prevent="setActiveModal">
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 
 export default {
   name: 'EditButton',
-
+  computed: {
+    ...mapState(['selectedHouseId', 'listing'])
+  },
   methods: {
     setActiveModal() {
       this.$store.dispatch('setActiveModal', true)
     },
-
+    setListing() {
+      this.$store.dispatch('setListing', parseInt(this.$store.state.selectedHouseId));
+    }
   },
 }
 </script>
